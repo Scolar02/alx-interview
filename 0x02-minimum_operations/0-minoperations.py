3d00283d9588f730036a40a470899d8b1fce14e4
Scolar02 <njugunascolar83@gmail.com>
2023-10-12 02:30:13 -0700

    pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,8 +6,16 @@ Returns: int: The minimum number of operations required.
 Copy all and paste.
 """
 
-
 def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to obtain n H characters.
+    
+    Args:
+        n (int): The target number of characters.
+
+    Returns:
+        int: The minimum number of operations required.
+    """
     t = 0
     m = 2
     while n > 1:
